d7c80c1e5b5cc539da272f2f825dfb61daaf2eb5
ViniciusFurtado1 <vinicius.furtado.sa@ccc.ufcg.edu.br>
2019-05-28 18:19:28 -0300

    validaCadastrarPessoaSemPartido

--- src/Controllers/ValidaSystemController.java
+++ src/Controllers/ValidaSystemController.java
@@ -2,14 +2,21 @@ package Controllers;
 
 public class ValidaSystemController {
 
-    public void validaCadastrarPartido(String partido) {
-        if (partido == null || "".equals(partido.trim())) {
-            throw new IllegalArgumentException("Erro ao cadastrar partido: partido nao pode ser vazio ou nulo");
+    public void validaCadastrarPessoaComPartido(String nome, String dni, String estado, String interesses, String partido) {
+        if (nome == null || "".equals(nome.trim())) {
+            throw new IllegalArgumentException("Erro ao cadastrar pessoa: nome nao pode ser vazio ou nulo");
         }
 
+        if (dni == null || "".equals(dni.trim())) {
+            throw new IllegalArgumentException("Erro ao cadastrar pessoa: dni nao pode ser vazio ou nulo");
+        }
+
+        if (estado == null || "".equals(estado.trim())) {
+            throw new IllegalArgumentException("Erro ao cadastrar pessoa: estado nao pode ser vazio ou nulo");
+        }
     }
 
-    public void validaCadastrarPessoa(String nome, String dni, String estado, String interesses, String partido) {
+    public void validaCadastrarPessoaSemPartido(String nome, String dni, String estado, String interesses) {
         if (nome == null || "".equals(nome.trim())) {
             throw new IllegalArgumentException("Erro ao cadastrar pessoa: nome nao pode ser vazio ou nulo");
         }
@@ -30,6 +37,12 @@ public class ValidaSystemController {
 
         if (dataDeInicio == null || "".equals(dataDeInicio.trim())) {
             throw new IllegalArgumentException("Erro ao cadastrar deputado: data nao pode ser vazio ou nulo");
+        }
+    }
+
+    public void validaCadastrarPartido(String partido) {
+        if (partido == null || "".equals(partido.trim())) {
+            throw new IllegalArgumentException("Erro ao cadastrar partido: partido nao pode ser vazio ou nulo");
         }
     }
 }
