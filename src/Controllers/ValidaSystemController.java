99f6fe47dc36981917c3286d9157488e6ac49d6a
ViniciusFurtado1 <vinicius.furtado.sa@ccc.ufcg.edu.br>
2019-05-28 17:57:38 -0300

    validaCadastrarDeputado

--- src/Controllers/ValidaSystemController.java
+++ src/Controllers/ValidaSystemController.java
@@ -7,4 +7,14 @@ public class ValidaSystemController {
             throw new IllegalArgumentException("Erro ao cadastrar partido: partido nao pode ser vazio ou nulo");
         }
     }
+
+    public void validaCadastrarDeputado(String dni, String dataDeInicio) {
+        if (dni == null || "".equals(dni.trim())) {
+            throw new IllegalArgumentException("Erro ao cadastrar pessoa: dni nao pode ser vazio ou nulo");
+        }
+
+        if (dataDeInicio == null || "".equals(dataDeInicio.trim())) {
+            throw new IllegalArgumentException("Erro ao cadastrar deputado: data nao pode ser vazio ou nulo");
+        }
+    }
 }
