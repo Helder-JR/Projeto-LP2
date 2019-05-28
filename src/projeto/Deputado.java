42687be8d818e1dbe6e273f36aa0cd2160769233
Vinicius Furtado <vinicius.furtado.sa@ccc.ufcg.edu.br>
2019-05-28 06:55:28 -0300

    Criação da Classe Deputado

---
+++ src/projeto/Deputado.java
@@ -0,0 +1,12 @@
+package projeto;
+
+public class Deputado {
+    private String dni;
+    private String dataDeInicio;
+    private int LeisAprovadas;
+
+    public Deputado(String dni, String dataDeInicio) {
+        this.dni = dni;
+        this.dataDeInicio = dataDeInicio;
+    }
+}
