ed793c9fbbbea6c173c06e5193e8b434acc93a25
PetrusKyrbb <50366245+PetrusKyrbb@users.noreply.github.com>
2019-05-28 19:26:24 -0300

    Validação do SystemController

--- src/Controllers/SystemController.java
+++ src/Controllers/SystemController.java
@@ -19,25 +19,28 @@ public class SystemController {
     }
 
     public boolean cadastrarPessoa(String nome, String dni, String estado, String interesses, String partido) {
+        this.validador.validaCadastrarPessoaComPartido(nome, dni, estado, interesses, partido);
         Pessoa pessoa = new Pessoa(nome, dni, estado, interesses, partido);
         this.cadastroPessoas.put(dni, pessoa);
         return true;
     }
 
     public boolean cadastrarPessoa(String nome, String dni, String estado, String interesses) {
+        this.validador.validaCadastrarPessoaSemPartido(nome, dni, estado, interesses);
         Pessoa pessoa = new Pessoa(nome, dni, estado, interesses);
         this.cadastroPessoas.put(dni, pessoa);
         return true;
     }
 
     public boolean cadastrarDeputado(String dni, String dataDeInicio) throws ParseException {
+        this.validador.validaCadastrarDeputado(dni, dataDeInicio);
         Deputado funcao = new Deputado(dni,dataDeInicio);
         this.cadastroPessoas.get(dni).setFuncao(funcao);
         return true;
     }
 
     public boolean cadastrarPartido(String partido) {
-        validador.validaCadastrarPartido(partido);
+        this.validador.validaCadastrarPartido(partido);
         this.partidosGovernistas.add(partido);
         return true;
     }
