1190587f5ef6054a3d29b34e96258fe77ed62945
PetrusKyrbb <50366245+PetrusKyrbb@users.noreply.github.com>
2019-05-30 21:28:12 -0300

    Update Civil.java

--- src/Entidades/Civil.java
+++ src/Entidades/Civil.java
@@ -16,15 +16,17 @@ public class Civil implements Funcao {
      * @return uma String referente a representação textual dessa pessoa.
      */
     public String toString(String nome, String dni, String estado, String partido, String interesses) {
-        if ("".equals(partido.trim()) && !"".equals(interesses.trim())) {
-            return String.format("%s - %s (%s) - Interesses: %s", nome, dni, estado, interesses);
-        }
-        if ("".equals(interesses.trim()) && !"".equals(partido.trim())) {
-            return String.format("%s - %s (%s) - %s", nome, dni, estado, partido);
-        }
-        if ("".equals(interesses.trim()) && "".equals(partido.trim())) {
-            return String.format("%s - %s (%s)", nome, dni, estado);
-        }
-        return String.format("%s - %s (%s) - %s - Interesses: %s", nome, dni, estado, partido, interesses);
+        String toString = String.format("%s - %s (%s) - %s - Interesses: %s", nome, dni, estado, partido, interesses);
+
+        if ("".equals(partido.trim()) && !"".equals(interesses.trim()))
+            toString = String.format("%s - %s (%s) - Interesses: %s", nome, dni, estado, interesses);
+
+        else if ("".equals(interesses.trim()) && !"".equals(partido.trim()))
+            toString = String.format("%s - %s (%s) - %s", nome, dni, estado, partido);
+
+        else if ("".equals(interesses.trim()) && "".equals(partido.trim()))
+            toString = String.format("%s - %s (%s)", nome, dni, estado);
+
+        return toString;
     }
 }
