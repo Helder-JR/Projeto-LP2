a0f91867848765a20e43616a4a8d3ac18049befa
Helder Chaves Leite Junior <helder.junior@ccc.ufcg.edu.br>
2019-06-03 09:24:04 -0300

    Atualização da documentação da classe Facade.

--- src/Controllers/Facade.java
+++ src/Controllers/Facade.java
@@ -68,34 +68,70 @@ public class Facade {
         this.ecoController.cadastrarPessoa(nome, dni, estado, interesses);
     }
 
+    /**
+     * Cadastra um(a) deputado(a) no sistema.
+     *
+     * @param dni o DNI da pessoa que será cadastrada como deputado(a).
+     * @param dataDeInicio a data de início do mandato dessa pessoa como deputado(a).
+     * @throws ParseException caso a data não esteja em um formato válido (dd/MM/yyyy).
+     */
     public void cadastrarDeputado(String dni, String dataDeInicio) throws ParseException {
         this.ecoController.cadastrarDeputado(dni, dataDeInicio);
     }
 
+    /**
+     * Cadastra um partido no sistema.
+     * 
+     * @param partido o partido que será cadastrado.
+     */
     public void cadastrarPartido(String partido) {
         this.ecoController.cadastrarPartido(partido);
     }
 
+    /**
+     * Exibe uma pessoa cadastrada no sistema.
+     * 
+     * @param dni o DNI da pessoa que será exibida.
+     */
     public String exibirPessoa(String dni) {
        return this.ecoController.exibirPessoa(dni);
     }
 
+    /**
+     * Exibe uma lista em ordem lexicográfica da base partidária cadastrada no sistema.
+     * 
+     * @return a String referente a lista de partidos cadastrados no sistema.
+     */
     public String exibirBase() {
         return this.ecoController.exibirBase();
     }
 
+    /**
+     * Armazena em um arquivo o estado atual do controlador do sistema.
+     */
     public void salvarSistema() {
         this.dados.salvar(this.ecoController,this.save);
     }
 
+    /**
+     * Carrega a partir de um arquivo um objeto controlador do sistema.
+     */
     public void carregarSistema() {
         this.ecoController = this.dados.carregar(this.save);
     }
 
+    /**
+     * Retorna (a partir de um arquivo) o controlador do sistema para o seu estado inicial.
+     */
     public void limparSistema() {
         this.ecoController = this.dados.carregar(this.reset);
     }
 
+    /**
+     * Método por onde todo o programa se inicia, carregando e executando os testes de aceitação.
+     * 
+     * @param args a classe responsável pelo início do sistema e os caminhos para os testes de aceitação.
+     */
     public static void main(String[] args) {
         args = new String[] {"Controllers.Facade", "acceptance_test/use_case_1.txt",
                                                    "acceptance_test/use_case_2.txt",
